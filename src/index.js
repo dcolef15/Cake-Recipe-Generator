04fc42522db70cbaf454e6e4bf6eb93146b07359
dcolef15 <diacolef@optonline.net>
2024-05-22 18:22:05 -0400

    Changed Poem context for AI

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "f4bbd2f6t053305e58789o06468fa4bf";
   let context =
-    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
-  let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
+    "You are a romanian Poem expert and love to write poems. You mission is to generate a poem . Sign the poem with 'SheCodes AI' <strong> element at the bottom of the poem and dont put at the top ";
+  let prompt = `User instructions: Generate a Romanian poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
