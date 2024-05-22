2e8f7e5560a2f367d879533b46e02d5babe1a3f0
dcolef15 <diacolef@optonline.net>
2024-05-22 14:33:50 -0400

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
+  console.log("generating recipe");
   new Typewriter("#recipe", {
-    strings: "Triple Chocolate Cake",
+    strings: response.data.answer,
     autoStart: true,
     delay: 15,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let insturctionsInput = document.querySelector("#user-instructions");
+  let apiKey = "f4bbd2f6t053305e58789o06468fa4bf";
+  let prompt = "User instructions: Generate recipes for a Cake";
+  let context =
+    "Find the recipes for a chocolate, vanilla,strawberry, and carrot cake. Doesnt have to be in order. Your mission is to generate a cake recipe and instructions for each in HTML. Make sure to follow user instructions.Sign the peom with 'SheCodes AI' inside <strong> element. at the bottom and not at the top.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating recipe");
+  console.log("Prompt: ${prompt}");
+  console.log("Context: ${context}");
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#Cake-Recipe-Generator-Form");
